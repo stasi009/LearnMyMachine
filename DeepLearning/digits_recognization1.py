6ac148e152a2855b239f1d424db93f0e0f9335fa
Stasi <cheka008@gmail.com>
2016-02-20 12:02:45 -0800

    rename module name

--- DeepLearning/digits_recognization1.py
+++ DeepLearning/digits_recognization1.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 # plt.style.use("ggplot")
 
 import commfuncs
-import MnistDataset
+import mnistdatas
 
-mnist = MnistDataset.MnistDataset("../datas")
+mnist = mnistdatas.MnistDataset("../datas")
 mnist.load_train()
 mnist.random_plot_different_digits()
 mnist.random_plot_same_digits(9)
