b4dcc1cfdd4021a74ad73000bb0e62f4da61faca
Chuanlin Zhao <cheka008@gmail.com>
2016-02-25 09:40:58 -0800

    import codes for reading MNIST dataset

--- UFLDL/UFLDL.py
+++ UFLDL/UFLDL.py
@@ -1,3 +1,13 @@
+﻿
+import numpy as np
+import matplotlib.pyplot as plt
+# plt.style.use("ggplot")
+from sklearn.metrics import accuracy_score
 
-import numpy
-import pandas
+import mnistdatas
+
+########################################## load train data
+mnist = mnistdatas.MnistDataset("../datas")
+mnist.load_train()
+mnist.random_plot_different_digits()
+mnist.random_plot_same_digits(9)
