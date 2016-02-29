cdd306728be3efbd4ef7b466439bb47bf8c51a85
Stasi <cheka008@gmail.com>
2016-02-29 09:21:04 -0800

    refurbish the codes

--- UFLDL/ex2_pca.py
+++ UFLDL/ex2_pca.py
@@ -4,26 +4,31 @@ import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 from pca import PCA
 
-datafile = "matlab/pcaData.txt"
+def demo_pca_2d():
+    datafile = "matlab/pcaData.txt"
+    X = np.loadtxt(datafile).T
 
-X = np.loadtxt(datafile).T
-# plt.scatter(X[:,0],X[:,1],marker="o",s=30,facecolors='none')
-plt.scatter(X[:,0],X[:,1])
+    pca = PCA(whiten=True)
+    Xwhiten = pca.fit_transform(X)
 
-pca = PCA(whiten=True)
-Xwhiten = pca.fit_transform(X)
-plt.plot([0,pca.V[0,0]],[0,pca.V[1,0]])
-plt.plot([0,pca.V[0,1]],[0,pca.V[1,1]])
+    plt.scatter(X[:,0],X[:,1])# in original X, each column is a feature
+    # each column in V is a new axis
+    for i in xrange(2):
+        newaxis = pca.V[:,i]
+        pntX = [-newaxis[0],newaxis[0]]
+        pntY = [-newaxis[1],newaxis[1]]
+        plt.plot(pntX,pntY,label="%d principle component"%(i+1))
+    plt.legend(loc="best")
+    plt.title("original datas and princple components")
+
+    plt.scatter(pca.Xprojected[:,0],pca.Xprojected[:,1])
+    plt.title("projected, axes have different scales")
+
+    plt.scatter(Xwhiten[:,0],Xwhiten[:,1])
+    plt.title("whitened, all axes have similar scales")
 
 
-plt.scatter(Xwhiten[:,0],Xwhiten[:,1])
-plt.scatter(pca.Xprojected[:,0],pca.Xprojected[:,1])
 
-
-pca = PCA(n_components=1, whiten=True)
-Xwhiten = pca.fit_transform(X)
-plt.scatter(Xwhiten[:,0],Xwhiten[:,1])
-plt.scatter(pca.Xprojected[:,0],pca.Xprojected[:,1])
 
 
 
