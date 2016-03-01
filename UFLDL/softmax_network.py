bfaf018844fc0a182e67bce954f384d0c6fab1db
Stasi <cheka008@gmail.com>
2016-03-01 13:30:13 -0800

    copy the sparse_autoencoder here

--- UFLDL/softmax_network.py
+++ UFLDL/softmax_network.py
@@ -107,7 +107,97 @@ class OutputBlock(object):
         num_samples = Y.shape[1]
         return (self.activation - Y) * self.activation * (1 - self.activation) / (float(num_samples))
 
-    
+class Network(object):
+
+    def __init__(self,n_features,n_hidden,l2,expected_rho,sparse_beta):
+        self._input = InputBlock(n_features,n_hidden,l2=l2)
+        self._hidden = HiddenBlock(n_hidden,n_features,l2=l2,expected_rho=expected_rho,sparse_beta=sparse_beta)
+        self._output = OutputBlock()
+
+    def __assign_weights(self,weights):
+        offset = 0
+        offset,self._input.W = commfuncs.extract_param_matrix(weights,offset,self._input.W)
+        offset,self._hidden.W = commfuncs.extract_param_matrix(weights,offset,self._hidden.W)
+        assert offset == len(weights)
+
+    def __cost(self,weights,X,Y):
+        """
+        X: [S,F]
+        Y: [O,S]
+        """
+        self.__assign_weights(weights)
+
+        output_from_input = self._input.feedforward(X)
+        output_from_hidden = self._hidden.feedforward(output_from_input)
+        self._output.feedforward(output_from_hidden)
+
+        return self._output.cost(Y) + self._input.penalty() + self._hidden.penalty()
+
+    def __cost_gradients(self,weights):
+        # ------------ feedforward to get cost
+        cost = self.__cost(weights,self.X,self.X.T)
+        
+        # ------------ backpropagate to get gradients
+        grad_output_input = self._output.backpropagate(self.X.T) # gradient on output_block's input
+        grad_hidden_input = self._hidden.backpropagate(grad_output_input) # gradient on hidden_block's input
+        self._input.backpropagate(grad_hidden_input)
+        
+        return cost,np.r_[self._input.grad_cost_w.flatten(),self._hidden.grad_cost_w.flatten()]
+
+    def weights_vector(self): return np.r_[self._input.W.flatten(),self._hidden.W.flatten()]
+
+    def fit(self,X,method="L-BFGS-B",maxiter=400):
+        self.X = X
+
+        options = {'maxiter': maxiter, 'disp': True}
+        result = scipy.optimize.minimize(self.__cost_gradients, self.weights_vector(), 
+                                         method=method, jac=True, options=options)
+
+        self.__assign_weights(result.x)
+
+    def __numeric_gradients(self,X, weights, epsilon):
+        total = len(weights)
+        gradients = np.zeros(total)
+
+        for index in xrange(total):
+            old_weight = weights[index]
+
+            weights[index] += epsilon
+            cost_plus = self.__cost(weights,X,X.T)
+
+            weights[index] -= 2 * epsilon
+            cost_minus = self.__cost(weights,X,X.T)
+
+            gradients[index] = (cost_plus - cost_minus) / (2 * epsilon)
+            weights[index] = old_weight
+            # print "%d/%d numeric gradients, %3.2f%% completes" % (index + 1,total,(index + 1) * 100.0 / total)
+
+        return gradients
+
+    def check_gradients(self,X, weights, epsilon=1e-4):
+        self.X = X
+        cost, analytic_gradients = self.__cost_gradients(weights)
+
+        numeric_gradients = self.__numeric_gradients(X,weights,epsilon)
+        # print "gradients, 1st column is analytic, 2nd column is numeric: \n%s" % (np.c_[analytic_gradients,numeric_gradients])
+
+        norm_difference = np.linalg.norm(numeric_gradients - analytic_gradients)
+        norm_numeric = np.linalg.norm(numeric_gradients)
+        norm_analytic = np.linalg.norm(analytic_gradients)
+        relative_error = norm_difference / (norm_numeric + norm_analytic)
+
+        if relative_error <= 1e-7:   
+            print('    OK:      relative error=%e' % relative_error)
+        elif relative_error <= 1e-4:      
+            print('*** WARNING: relative error=%e' % relative_error)
+        else:                        
+            raise Exception('!!! PROBLEM: relative error=%e' % relative_error)
+
+    def visualize_meta_features(self,pic_name):
+        # W is a [H,F+1] matrix
+        meta_features = self._input.W[:,1:].transpose() # [F,H] matrix
+        # display_image_patch will treat each column as a single image patch
+        display.display_image_patches(meta_features,pic_name)
 
 
 
