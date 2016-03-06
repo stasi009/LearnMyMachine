8866cfe2487e632bad10b3493430c25f441c641f
Stasi <cheka008@gmail.com>
2016-03-06 10:25:28 -0800

    provide the option to PCA on covariance matrix, which saves memory

--- UFLDL/pca.py
+++ UFLDL/pca.py
@@ -8,19 +8,27 @@ class SimplePCA(object):
         self.whiten = whiten
         self.__epsilon = 1e-6
 
-    def fit_transform(self,X):
+    def fit_transform(self,X, method="svd"):
         """
         X: [n_samples,n_features]
+        method="svd", SVD on the orignal/raw matrix, saving the complexity calculating covariance matrix
+        method="evd", Eigen-Value Decomposition on covariance matrix, dealing a d*d matrix, use less memory
         """
         feature_mean = X.mean(axis=0)
         # although only "center mean" is necessary, we also need to have unit
-        # variance
-        # to ignore the impact caused by different unit
+        # variance, to ignore the impact caused by different unit
         feature_std = X.std(axis=0)
-        self.Xnormalized = (X - feature_mean) / feature_std
+        self.Xnormalized = (X - feature_mean) / feature_std 
 
-        U,self.S,Vt = np.linalg.svd(self.Xnormalized)
-        self.V = Vt.T
+        if matrix.lower() == "svd":
+            U,self.S,Vt = np.linalg.svd(self.Xnormalized)
+            self.V = Vt.T
+        elif matrix.lower() == "evd":
+            covariance =  self.Xnormalized.T.dot(self.Xnormalized)
+            self.V,S, _ = np.linalg.svd(covariance)
+            self.S = np.sqrt(S)
+        else:
+            raise Exception("unknown method")
 
         if self.n_components is None:
             self.n_components = X.shape[1]# use all features, no reduction
