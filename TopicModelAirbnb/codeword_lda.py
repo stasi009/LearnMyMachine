19dc38e5a3652da13eb2469fb63130b0d877f19d
stasi009 <stasi009@163.com>
2016-11-05 10:47:33 +0800

    save

--- TopicModelAirbnb/codeword_lda.py
+++ TopicModelAirbnb/codeword_lda.py
@@ -1,113 +1,51 @@
 
 import logging
-import re
-import pandas as pd
-import text_utility
 from review import ReviewsDAL
 from gensim import corpora,models,matutils
 import nltk
+from word_coder import WordCoder
 
-PosReview = 'POSREVIEW'
-NegReview = 'NEGREVIEW'
+def words_stream():
+    word_coder = WordCoder()
 
-class WordsStream(object):
-    PosWords = frozenset( pd.read_csv('extend_poswords.txt',header=None).values.flatten() )
-    NegWords = frozenset( pd.read_csv('extend_negwords.txt',header=None).values.flatten() )
+    dal = ReviewsDAL()
+    review_stream = dal.load_words()
+    for index, r in enumerate(review_stream):
+        yield word_coder.code(r.sent.words)
 
-    def append_sentiment_words(self,words):
-        extra_words = []
-        for w in words:
-            if w in WordsStream.PosWords:
-                extra_words.append(PosReview)
-                # print "+++ {}: Positive".format(w)
+        if index % 300 == 0:
+            print "{} examples loaded from mongodb".format(index + 1)
 
-            if w in WordsStream.NegWords:
-                extra_words.append(NegReview)
-                # print "--- {}: Negative".format(w)
-
-        words += extra_words
-
-    def stream(self):
-        dal = ReviewsDAL()
-        review_stream = dal.load_words()
-        for index, r in enumerate(review_stream):
-
-            self.append_sentiment_words(r.sent.words)
-            yield r.sent.words
-
-            if index % 300 == 0:
-                print "{} examples loaded from mongodb".format(index + 1)
-
-        dal.close()
+    dal.close()
 
 def build_dictionary():
-    words_stream = WordsStream().stream()
+    wstream = words_stream()
 
-    dictionary = corpora.Dictionary(words_stream)
+    dictionary = corpora.Dictionary(wstream)
     dictionary.save('codeword_dictionary.dict')  # store the dictionary, for future reference
     dictionary.save_as_text("codeword_dictionary.txt", sort_by_word=False)
 
     print "======== Dictionary Generated and Saved ========"
 
-def load_extra_stopwords():
-    extra_stop_words = set()
-    with open('extra_stopwords.txt','rt') as inf:
-        for line in inf:
-            w = line.strip()
-            if len(w) >= 1:
-                extra_stop_words.add(w)
-                extra_stop_words.add(w + text_utility.NEG_SUFFIX)
-    return extra_stop_words
+def clean_dict_save(no_below=10, keep_n=100000):
+    dictionary = corpora.Dictionary.load('codeword_dictionary.dict')
+    print "originally, there are {} tokens".format(len(dictionary))
 
-class DictCleaner(object):
-    def __init__(self):
-        self._invalid_pattern = re.compile(r"[^a-zA-Z_\-]")
-        self._extra_stop_words = load_extra_stopwords()
+    # !!! we cannot filter out 'too frequent'
+    # !!! because 'POSREVIEW' and 'NEGREVIEW' are just two most frequent words
+    dictionary.filter_extremes(no_below=no_below, no_above=1, keep_n=keep_n)
+    print "after filtering too rare, there are {} tokens".format(len(dictionary))
 
-    def is_token_invalid(self,token):
-        if self._invalid_pattern.search(token) is not None:
-            return True
-
-        if token in self._extra_stop_words:
-            return True
-
-        return False
-
-    def clean(self,no_below=5, keep_n=100000):
-        dictionary = corpora.Dictionary.load('codeword_dictionary.dict')
-        print "originally, there are {} tokens".format(len(dictionary))
-
-        # !!! we cannot filter out 'too frequent'
-        # !!! because 'POSREVIEW' and 'NEGREVIEW' are just two most frequent words
-        dictionary.filter_extremes(no_below=no_below,no_above=1,keep_n=keep_n)
-        print "after filtering too rare, there are {} tokens".format(len(dictionary))
-
-        # filter out invalid tokens
-        invalid_tokenids = [id for token,id in dictionary.token2id.viewitems()
-                            if self.is_token_invalid(token) ]
-        print "there are {} tokens are invalid".format(len(invalid_tokenids))
-
-        dictionary.filter_tokens(bad_ids = invalid_tokenids)
-        print "after filtering invalid, there are {} tokens".format(len(dictionary))
-
-        return dictionary
-
-def clean_dict_save():
-    cleaner = DictCleaner()
-    clean_dict = cleaner.clean(no_below=10)
-
-    clean_dict.save('codeword_dictionary.dict')
+    dictionary.save('codeword_dictionary.dict')
     # sort by decreasing doc-frequency
-    clean_dict.save_as_text("codeword_dictionary.txt", sort_by_word=False)
-
+    dictionary.save_as_text("codeword_dictionary.txt", sort_by_word=False)
     print "##################### dictionary is cleaned, shrinked and saved"
 
 def build_bow_save():
     dictionary = corpora.Dictionary.load('codeword_dictionary.dict')
 
     print "\n=========== begin BOW, ......"
-    wstream = WordsStream()
-    bow_stream = (dictionary.doc2bow(words) for words in wstream.stream())
+    bow_stream = (dictionary.doc2bow(words) for words in words_stream())
 
     target_file = "codeword_airbnb.bow"
     corpora.MmCorpus.serialize(target_file, bow_stream)
@@ -164,10 +102,10 @@ if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
     # build_dictionary()
-
     # clean_dict_save()
     # build_bow_save()
-    run_lda(100)
+
+    run_lda(30,parallel=False)
 
 
 
